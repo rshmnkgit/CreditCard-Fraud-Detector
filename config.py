1c38111b66deeb29b0394570992ee904a53c99be
rshmnkgit <64658801+rshmnkgit@users.noreply.github.com>
2020-11-19 13:49:15 -0500

    config.py update

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-username = "rhmphjkssxvhxs"
-passowrd = "2ff88b3da4fa0b6f2f421d7b5aa5361447957c22ad5d73ffb979bbeb47007aaf"
+username = "Your Username Here"
+password = "Your Password Here"
+dbname = "Your Heroku Postgres Database Name Here"
+host = "YYour Heroku Postgres Host Name Here"
