ba88097b80740cc2bef00a8520d90b397cc02a59
rshmnkgit <64658801+rshmnkgit@users.noreply.github.com>
2021-03-31 13:39:32 -0400

    config file updated

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
-username = "rhmphjkssxvhxs"
-password = "2ff88b3da4fa0b6f2f421d7b5aa5361447957c22ad5d73ffb979bbeb47007aaf"
-dbname = "d38ov1t1kmn4vq"
-host = "ec2-52-87-22-151.compute-1.amazonaws.com"
-
-
 #username = "Your Username Here"
 #password = "Your Password Here"
 #dbname = "Your Heroku Postgres Database Name Here"
